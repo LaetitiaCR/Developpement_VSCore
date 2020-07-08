23c5c4dfae8f7e7cf075edcad8022f272dd415b8
LaetitiaCR <webmaster.Laetitia@laposte.net>
2020-07-08 17:20:26 +0200

    Initial commit

---
+++ CompteBancaire/CompteBancaire/Compte.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net.Security;
+
+namespace CompteBancaire
+{
+    class Compte
+    {
+
+        private int decouvertAutorise;
+        private string nom;
+        private int numero;
+        private int solde;
+        private int v1;
+        private string v2;
+        private int v3;
+        private int v4;
+        private String msg;
+
+       
+
+        public Compte()
+        {
+
+        }
+        public Compte(int _numero, string _nom, int _solde, int _decouvertAutorise)
+        {
+            numero = _numero;
+            nom = _nom;
+            solde = _solde;
+            decouvertAutorise = _decouvertAutorise;
+        }
+
+        public void Crediter(int _montant)
+        {
+            if (decouvertAutorise < 0 && decouvertAutorise + _montant < 0) {
+                msg = "Le retrait ne peut être effectuer";
+                //ToString(msg); 
+            }
+            else{
+                msg = "Vous avez retiré " + _montant + " euros et votre nouveau solde est : " + solde;
+                //Console.WriteLine(msg);
+            }
+        }
+        public bool Debiter(int _montant)
+        {
+           solde = solde + _montant;
+           return true;
+        }
+        //public Boolean Superieur(Compte _autreCompte)
+        //{
+
+        //}
+        public string ToString()
+        {
+            return msg;
+        }
+        //public Boolean transferer(int _montant, Compte _compteDestinataire)
+        //{
+
+        //}
+    }
+}
+
---
+++ CompteBancaire/CompteBancaire/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompteBancaire
+{
+    class Program
+    {
+       static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+            //Compte c = new Compte();
+            //Compte c1 = new Compte(12345, "toto", 1000, -500);
+            Compte c1 = new Compte(12345, "toto", 3000, -500);
+            //Console.WriteLine(c.ToString());
+            c1.Crediter(100);
+            Console.WriteLine(c1.ToString());
+        }
+    }
+
+}
