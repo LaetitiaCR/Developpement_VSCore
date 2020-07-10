51a39a6fe144c28b6e80ffbc524cf9ff4ae3cc7f
LaetitiaCR <webmaster.Laetitia@laposte.net>
2020-07-10 11:57:37 +0200

    correction compte

--- CompteBancaire/CompteBancaire/Compte.cs
+++ CompteBancaire/CompteBancaire/Compte.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Net.Security;
 
 namespace CompteBancaire
@@ -11,11 +12,7 @@ namespace CompteBancaire
         private string name;
         private int numero;
         private int solde;
-        private int v1;
-        private string v2;
-        private int v3;
-        private int v4;
-        private string msg;
+        //private string msg;
 
        
 
@@ -32,57 +29,63 @@ namespace CompteBancaire
         }
 
      
-
+        //ajouter de l'argent sur son compte
         public void Crediter(int _montant)
         {
-            if (decouvertAutorise < 0 && decouvertAutorise + _montant < 0) {
-                msg = "Le retrait ne peut être effectuer";
-           
-            }
-            else{
-                solde = solde - _montant; 
-                msg = "Vous avez retiré " + _montant + " euros et votre nouveau solde est : " + solde;
-                
-                //Console.WriteLine(msg);
-            }
-            //ToString(msg);
+            solde = solde + _montant;
+              
 
         }
+
+        //enlever de l'argent sur son compte
         public bool Debiter(int _montant)
         {
-            solde = solde + _montant;
-            msg = "Vous avez ajouté " + _montant + " euros. Votre solde est à présent de : " + solde + " euros";
-           // ToString(msg);
-            return true;
-        }
-        
-        public Boolean Superieur(Compte _autreCompte)
-        {
-            if (_autreCompte.solde < solde) {
-                msg = "Le solde du compte passé en paramètre est inférieur";
-               
-            }
-            else if (_autreCompte.solde == solde){
-                msg = "Les soldes des deux comptes sont identiques";
+            if (decouvertAutorise < 0 && decouvertAutorise + _montant < 0)
+            {
+                //msg = "Le retrait ne peut être effectuer";
+                return false;
             }
             else
             {
-                msg = "Le solde du compte passé en paramètre est supérieur";
+                solde = solde - _montant;
+                return true;
+                //msg = "Vous avez retiré " + _montant + " euros et votre nouveau solde est : " + solde;
+
+               
+            }
+           
+        }
+
+        public Boolean Superieur(Compte _autreCompte)
+        {
+            if (_autreCompte.solde < solde) {
+                // msg = "Le solde du compte passé en paramètre est inférieur";
+                return false;
+               
+            }
+            else if (_autreCompte.solde == solde){
+                //msg = "Les soldes des deux comptes sont identiques";
+                return false;
+            }
+            else
+            {
+                //msg = "Le solde du compte passé en paramètre est supérieur";
+                return true;
             }
 
-            return true;
+         
         }
 
         public override string ToString()
         { 
-            return msg;
+            return "Le numero de compte est " + numero + " de M. ou Mme " + name + " avec un solde de : " + solde;
         }
         
         public Boolean Transferer(int _montant, Compte _compteDestinataire)
         {
             _compteDestinataire.Crediter(_montant);
             solde = solde + _montant;
-            msg = "Bonjour M ou Mme " + name + " Suite à votre transfert, le solde de votre compte est à présent de : " + solde;
+           // msg = "Bonjour M ou Mme " + name + " Suite à votre transfert, le solde de votre compte est à présent de : " + solde;
             return true;
         }
     }
--- CompteBancaire/CompteBancaire/Program.cs
+++ CompteBancaire/CompteBancaire/Program.cs
@@ -2,6 +2,8 @@
 
 namespace CompteBancaire
 {
+
+    
     class Program
     {
        static void Main(string[] args){
@@ -11,20 +13,37 @@ namespace CompteBancaire
             //Compte c1 = new Compte(12345, "toto", 1000, -500);
             Compte c1 = new Compte(12345, "toto", 3000, -500);
             Compte c2 = new Compte(45678, "titi", 1200, 1500);
+
             
-            //deduire 100
+            //ajouter 100
+            //int montant = 100;
             c1.Crediter(100);
             Console.WriteLine(c1.ToString());
-            //ajouter 200
+            //string msg = "Vous avez ajouté " + montant + " euros. Votre solde est à présent de : " + solde + " euros";
+
+            //deduire 200
             if (c1.Debiter(200) == true)
             {
                 Console.WriteLine(c1.ToString());
-            };
+            }
+            else
+            {
+                Console.WriteLine("Le retrait n'a pas été effectué");
+            }
 
-
+            if (c1.Superieur(c2) == true)
+            {
+                Console.WriteLine("Le solde du compte c1 est supérieur au solde du compte c2");
+            }
+            else {
+                Console.WriteLine("Le solde du compte c1 est inférieur ou égale au solde du compte c2");
+            }
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
 
             c1.Transferer(3300, c2);
             Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
         }
     }
 
